281eab0bbcbeb9c6eba18dcdad23403d2da06bca
chanyu1 <haneul1ij@gmail.com>
2023-08-12 17:32:44 +0900

    feat: post list

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,12 +1,29 @@
 'use client';
 
 import useSWR from 'swr';
+import { GridLoader } from 'react-spinners';
 import { SimplePost } from './model/post';
+import PostListCard from './PostListCard';
 
 export default function PostList() {
   const { data: posts, isLoading } = useSWR<SimplePost[]>('/api/posts');
 
   return (
-    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+    <section>
+      {isLoading && (
+        <div>
+          <GridLoader color="red" />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
